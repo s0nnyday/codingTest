604371b52f3f8782b2d2e59c848bf2ba7f2c5e1b
손지민 <135934401+s0nnyday@users.noreply.github.com>
2023-08-30 22:30:08 +0900

    [level 1] Title: 자릿수 더하기, Time: 6.53 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String num = n+"";
+        for(int i=0;i<num.length();i++) {
+        	answer += num.charAt(i)-'0';
+        }
+        return answer;
+    }
+}
