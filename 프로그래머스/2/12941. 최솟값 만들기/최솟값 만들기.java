99a10ad7cbf5423f37adee7790c693a3d8430632
손지민 <lijnar100@naver.com>
2024-03-25 11:52:43 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.45 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int sum = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+
+        
+        for(int i=0; i<A.length; i++) {
+            sum += A[i]*B[A.length-i-1];
+        }
+        
+        System.out.println(sum);
+
+        return sum;
+    }
+}
