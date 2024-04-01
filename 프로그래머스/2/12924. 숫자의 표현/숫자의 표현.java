2c8a9229521d4fd30cdb3cfbbd7532ec849f49b9
손지민 <lijnar100@naver.com>
2024-04-01 15:48:25 +0900

    [level 2] Title: 숫자의 표현, Time: 1.40 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,28 +1,21 @@
 class Solution {
     public int solution(int n) {
-        int count = 0; // 자기 자신은 포함이니까
+        int count = 0;
         
         for (int i=1; i<=n; i++) { //i=1~8까지 반복
             int sum = 0;
-            // System.out.println("i 시작: " + i);
             
             for (int j=i;j<=n;j++) {
-                // System.out.println("j 시작: "+ j);
                 sum+=j;
-                // System.out.println("sum: " + sum);
-                if(sum==n) { // sum이 n 이랑 같으면 count++;
+                if(sum==n) { // sum이 n 이랑 같으면 count 추가o
                     count++;
-                    //System.out.println("count 증가 o: " + count);
                     break;
                 }
-                if(sum>n) { // sum 이 n 넘으면 종료;
-                    //System.out.println("count 증가 x:" + count);
+                if(sum>n) { // sum이 n 넘으면 종료 count 추가x
                     break;
                 }
-                // System.out.println("--j반복문 끝--");
             }
             
-            // System.out.println("--i반복문 끝--");
         }
         return count;
     }
