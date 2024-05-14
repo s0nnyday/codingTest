de7ddd93f61145f4f7e9271dafcd16c6904d0526
손지민 <lijnar100@naver.com>
2024-05-14 22:32:19 +0900

    [level 2] Title: 짝지어 제거하기, Time: 43.68 ms, Memory: 59.5 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -4,12 +4,12 @@ class Solution {
         
         for (char c : s.toCharArray()) {
             if (sb.length() > 0 && sb.charAt(sb.length() - 1) == c) {
-                sb.deleteCharAt(sb.length() - 1); // 짝을 이루는 문자 제거
+                sb.deleteCharAt(sb.length() - 1);
             } else {
-                sb.append(c); // 짝을 이루지 않는 경우 문자 추가
+                sb.append(c);
             }
         }
         
-        return sb.length() == 0 ? 1 : 0; // 모두 제거되었는지 여부 확인 후 반환
+        return sb.length() == 0 ? 1 : 0;
     }
 }
